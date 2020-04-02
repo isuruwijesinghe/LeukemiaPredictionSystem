89e6d6c680e8b3ac7589b937b075e7dbf28b776c
isuru <isuru@wadinternational.net>
2020-04-03 00:20:01 +0530

    aws s3 storage implemented and textit.biz sms gateway implemented.

--- app/Jobs/SendReportProcess.php
+++ app/Jobs/SendReportProcess.php
@@ -52,14 +52,43 @@ class SendReportProcess implements ShouldQueue
         $canvas->page_text(275, 760, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
         //saving pdf with a generated number
         $file_name = $report->pdf_name . '.pdf';
-        Storage::disk('public')->put($file_name, $pdf->output());
-        // get pdf path    
-        // $file_url = Storage::disk('public')->url($file_name);
-        $file_url = Storage::url($file_name);
-        // $file_url = 'file:///IIT/FYP/blog/storage/app/public/' . $file_name;
+
+        // store local
+        // Storage::disk('public')->put($file_name, $pdf->output());
+        // // get pdf path    
+        // $file_url = Storage::url($file_name);
+
+        //uploading pdf to s3 bucket
+        Storage::disk('s3')->put($file_name, $pdf->output());
+        // get pdf path from s3 bucket
+        $file_url = Storage::disk('s3')->url($file_name);
         
         //update url
         $report->update(['pdf_url' => $file_url]);
+
+        
+        // sending sms with textit.biz
+        $user = "94719309953";
+        $password = "8458";
+        $text = urlencode('Check your report ' . $file_url);
+        $to = $report->patient->mobile_number;
+        // $to = "94719309953";
+        
+        
+        $baseurl ="http://www.textit.biz/sendmsg";
+        $url = "$baseurl/?id=$user&pw=$password&to=$to&text=$text";
+        $ret = file($url);
+        
+        $res= explode(":",$ret[0]);
+        
+        if (trim($res[0])=="OK")
+        {
+            echo "Message Sent - ID : ".$res[1];
+        }
+        else
+        {
+            echo "Sent Failed - Error : ".$res[1];
+        }
 
         }catch (\Throwable $th) {
               dd($th);
